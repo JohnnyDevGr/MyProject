194026c465ec923e892671fab0ec63893c30e22d
JohnnyDevGr <115351376+JohnnyDevGr@users.noreply.github.com>
2023-01-28 00:18:18 +0200

    validation form

--- script.js
+++ script.js
@@ -1,30 +1,72 @@
 /*    CONTACT FORM     */
 
-const fname = document.getElementById("fname");
+function validateForm() {
+  {
+    testName();
+    testLname();
+    testVEmail();
+    testEmail();
+    testphone();
+  }
+  function testName() {
+    let regName = /^[A-Za-z]+$/;
+    let fname = document.getElementById("fname").value;
+    if (regName.test(fname)) {
+      return true;
+    } else {
+      alert("Invalid first name");
+      return false;
+    }
+  }
 
-if (fname.value.trim().length === 0) {
-  alert("First name missing");
-}
-const lname = document.getElementById("lname");
+  function testLname() {
+    let regName = /^[A-Za-z]+$/;
+    let lname = document.getElementById("lname").value;
+    if (regName.test(lname)) {
+      return true;
+    } else {
+      alert("Invalid last name");
+      return false;
+    }
+  }
 
-if (lname.value.trim().length === 0) {
-  alert("Last name missing");
+  function testEmail() {
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let email = document.getElementById("email").value;
+    if (emailRegex.test(email)) {
+      return true;
+    } else {
+      alert("Invalid email address");
+      return false;
+    }
+  }
+  function testVEmail() {
+    let vemailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let vemail = document.getElementById("vemail").value;
+    if (vemailRegex.test(vemail)) {
+      return true;
+    } else {
+      alert("Invalid email address");
+      return false;
+    }
+  }
+  function testphone() {
+    let phoneRegex = /^[0-9]+$/;
+    let phone = document.getElementById("phone").value;
+    if (phoneRegex.test(phone)) {
+      return true;
+    } else {
+      alert("Invalid phone number");
+      return false;
+    }
+  }
+
+  let address = document.getElementById("address").value;
+
+  if (address.trim().length === 0) {
+    alert("Address is missing");
+    return false;
+  }
 }
 
-const email = document.getElementById("email");
-if (email.value.trim().length === 0) {
-  alert("Email missing");
-} else if (email.value.indexOf("@") < 0) {
-  alert("Invalid email address");
-}
-const vemail = document.getElementById("vemail");
-if (vemail.value.trim().length === 0) {
-  alert("Email missing");
-} else if (vemail.value.indexOf("@") < 0) {
-  alert("Invalid email address");
-}
-const address = document.getElementById("address");
-
-if (address.value.trim().length === 0) {
-  alert("Address is missing");
-}
+/*            SLIDESHOW            */
