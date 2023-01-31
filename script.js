400e7383380c72ee378ea7ecc123bbb1d49200d0
JohnnyDevGr <115351376+JohnnyDevGr@users.noreply.github.com>
2023-01-31 20:37:20 +0200

    navbar change,fill with content destinations
    αλλαγη Navbar, φτιαξιμο σιγα σιγα σελιδας destinations,αλλαγη σε αγγλικα ξανα,προσθηκη slideshow και contact form.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+"use strict";
+//ts@check
+
 /*    CONTACT FORM     */
 
 function validateForm() {
@@ -70,3 +73,106 @@ function validateForm() {
 }
 
 /*            SLIDESHOW            */
+let slideIndex = 1;
+let slides = document.getElementsByClassName("slides");
+let dots = document.getElementsByClassName("dot");
+let playButton = document.getElementById("play");
+let stopButton = document.getElementById("stop");
+let slideInterval;
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  slideInterval = setTimeout(showSlides, 3000);
+}
+
+function prevSlide() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex--;
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function nextSlide() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function currentSlide(index) {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex = index;
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function playSlide() {
+  slideInterval = setTimeout(showSlides, 3000);
+  playButton.style.display = "none";
+  stopButton.style.display = "block";
+}
+
+function stopSlide() {
+  clearTimeout(slideInterval);
+  stopButton.style.display = "none";
+  playButton.style.display = "block";
+}
+
+// Set default style of images to "none"
+for (let i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+
+// Show the first image
+slides[0].style.display = "block";
+dots[0].className += " active";
+playButton.style.display = "none";
+showSlides();
+
+/*        NAVBAR TABS      */
+
+// Select all tabs
+const tabs = document.querySelectorAll(".tab");
+
+// Add click event listener to each tab
+tabs.forEach((tab) => {
+  tab.addEventListener("click", function () {
+    // Remove active class from all tabs
+    tabs.forEach((tab) => tab.classList.remove("active"));
+
+    // Add active class to the clicked tab
+    this.classList.add("active");
+  });
+});
