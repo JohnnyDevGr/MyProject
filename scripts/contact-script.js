a87072b42198a4315d869c390bfc5f8ead46cff4
JohnnyDevGr <115351376+JohnnyDevGr@users.noreply.github.com>
2023-03-04 00:08:42 +0200

    ftiaksimo validation form kai responsive
     3/3 oli mera

--- scripts/contact-script.js
+++ scripts/contact-script.js
@@ -3,91 +3,72 @@
 
 /*          CONTACT FORM       */
 
-const form = document.getElementsByClassName("form");
-const thankYou = document.getElementsByClassName("thank-you");
-const nameInput = document.querySelector('input[fname="fullname"]');
+function validateForm() {
+  const fname = document.getElementById("fname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const vemail = document.getElementById("vemail").value.trim();
+  const phone = document.getElementById("phone").value.trim();
+  const address = document.getElementById("address").value.trim();
 
-let isFormValid = false;
-
-const validateInputs = () => {
-  nameInput.classList.remove("invalid");
-  nameInput.nextElementSibling.classList.add("hidden");
-  isFormValid = true;
-  if (!nameInput.value) {
-    nameInput.classList.add("invalid");
-    nameInput.nextElementSibling.classList.remove("hidden");
-    isFormValid = true;
-  }
-};
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  validateInputs();
-  if (isFormValid) {
-    form.remove();
-    thankYou.classList.remove("hidden");
-  }
-});
-nameInput.addEventListener("input", () => {
-  validateInputs();
-});
-
-/* function validateForm() {
-  {
-    testName();
-    testVEmail();
-    testEmail();
-    testphone();
-  }
-
-
-
-  function testName() {
-    let regName = /^[A-Za-z]+$/;
-    let fname = document.getElementById("fname").value;
-    if (regName.test(fname)) {
-      return true;
-    } else {
-      alert("Invalid full name");
-      return false;
-    }
-  }
-
-  function testEmail() {
-    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let email = document.getElementById("email").value;
-    if (emailRegex.test(email)) {
-      return true;
-    } else {
-      alert("Invalid email address");
-      return false;
-    }
-  }
-  function testVEmail() {
-    let vemailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let vemail = document.getElementById("vemail").value;
-    if (vemailRegex.test(vemail)) {
-      return true;
-    } else {
-      alert("Invalid email address");
-      return false;
-    }
-  }
-  function testphone() {
-    let phoneRegex = /^[0-9]+$/;
-    let phone = document.getElementById("phone").value;
-    if (phoneRegex.test(phone)) {
-      return true;
-    } else {
-      alert("Invalid phone number");
-      return false;
-    }
-  }
-
-  let address = document.getElementById("address").value;
-
-  if (address.trim().length === 0) {
-    alert("Invalid Address");
+  if (!testName(fname)) {
+    alert("Invalid full name");
     return false;
   }
+
+  if (!testEmail(email)) {
+    alert("Invalid email address");
+    return false;
+  }
+
+  if (!testEmail(vemail)) {
+    alert("Invalid email address");
+    return false;
+  }
+
+  if (!testPhone(phone)) {
+    alert("Invalid phone number");
+    return false;
+  }
+
+  if (address.length === 0) {
+    alert("Invalid address");
+    return false;
+  }
+
+  return true;
 }
- */
+
+function testName(name) {
+  const regName = /^[A-Za-z]+$/;
+  return regName.test(name);
+}
+
+function testEmail(email) {
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailRegex.test(email);
+}
+
+function testPhone(phone) {
+  const phoneRegex = /^[0-9]+$/;
+  return phoneRegex.test(phone);
+}
+
+function submitForm(event) {
+  event.preventDefault(); // prevent the form from submitting
+
+  if (!validateForm()) {
+    return;
+  }
+
+  const form = document.getElementById("contact-form");
+  form.classList.add("hidden");
+
+  const thankYouMessage = document.getElementById("thank-you-message");
+  thankYouMessage.classList.remove("hidden");
+
+  document.getElementById("fname").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("vemail").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("address").value = "";
+}
